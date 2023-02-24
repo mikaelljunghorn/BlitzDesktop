26a71377a091ab386dd36e58ca1d8db0446b7d35
Mikael Ljunghorn <mikael_ljunghorn@hotmail.com>
2023-02-24 17:55:31 +0100

    Just version number update.

--- BlitzDesktop/My Project/AssemblyInfo.vb
+++ BlitzDesktop/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("22.12.2.0")>
-<Assembly: AssemblyFileVersion("22.12.2.0")>
+<Assembly: AssemblyVersion("23.02.1.0")>
+<Assembly: AssemblyFileVersion("23.02.1.0")>
